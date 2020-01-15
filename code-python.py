ee0f524c3b039bc2376914e26630e8d32610950b
blesseux <59804590+blesseux@users.noreply.github.com>
2020-01-15 10:53:43 +0100

    Update code-python.py

--- code-python.py
+++ code-python.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3.4
 
 import random
-r=random.random() * 100
-print(r)
-return(r)
+result=random.random() * 100
+print(result)
+return(result)
+import requests, json
+
+x = {}
+x["id-p"] = "RABBITMQ"
+x["id-t"] = "1"
+x["result"]= result
+myparam = {"data" : json.dumps(x)}
+r = requests.post("http://192.168.59.241:5000/rabbit/TODO",data=myparam)
+print("statut : {}".format(r.status_code))
+print(r.text)
