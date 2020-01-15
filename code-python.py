b3c4ddc12ef185c00f84fef353fc4f03fe2fe01f
blesseux <59804590+blesseux@users.noreply.github.com>
2020-01-15 11:11:55 +0100

    Update code-python.py

--- code-python.py
+++ code-python.py
@@ -11,5 +11,4 @@ x["id-t"] = "1"
 x["result"]= result
 myparam = {"data" : json.dumps(x)}
 r = requests.post("http://192.168.59.241:5000/rabbit/DONE",data=myparam)
-print("statut : {}".format(r.status_code))
-print(r.text)
+print(RESULTAT ENVOYE)
